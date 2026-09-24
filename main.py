8d31e477887a708c75502c3d438fbd9148375ae4
YAHYABARRI <yahyabarri25@gmail.com>
2026-09-24 19:34:25 +0000

    inngest setup

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-def main():
-    print("Hello from rag!")
+import logging
+
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-if __name__ == "__main__":
-    main()
+# Client Inngest
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+)
+
+
+# Fonction Inngest
+@inngest_client.create_function(
+    fn_id="rag-ingest-pdf",
+    trigger=inngest.TriggerEvent(
+        event="rag/ingest_pdf",
+    ),
+)
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return {"message": "PDF ingestion started"}
+
+
+# Application FastAPI
+app = FastAPI()
+
+
+# Expose les fonctions Inngest
+inngest.fast_api.serve(
+    app,
+    inngest_client,
+    [rag_ingest_pdf],
+)
